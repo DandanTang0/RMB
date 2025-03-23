cf8daaecafd462052456cdad0c554352c62d8852
Dandan Tang <150409008+DandanTang0@users.noreply.github.com>
2025-03-22 23:39:54 -0400

    Add files via upload

---
+++ doc/RMB.R
@@ -0,0 +1,10 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+library(RMB)
+
+## ----eval=FALSE---------------------------------------------------------------
+# set.seed(123)
+# data <- matrix(rnorm(200), 20, 10)
+# result <- RMB("MAR", data, seed = 123)
+# traceplot(result)
+
